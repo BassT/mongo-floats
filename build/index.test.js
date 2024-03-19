1e99dca41884c2442d8eed5d2a7796baae26dd55
Sebastian Richter <sebastian.max.richter@gmail.com>
2024-03-19 20:45:10 +0100

    Assert using epsilon instead of deep strict equal

--- build/index.test.js
+++ build/index.test.js
@@ -7,7 +7,13 @@ const node_test_1 = __importDefault(require("node:test"));
 const _1 = require(".");
 const node_assert_1 = __importDefault(require("node:assert"));
 (0, node_test_1.default)("should work correctly", () => {
-    const array = new Array(1000).fill(0.5);
+    const array = new Array(100000).fill(0).map(() => Math.random());
     const binary = (0, _1.toBinary)(array);
-    node_assert_1.default.deepStrictEqual(array, (0, _1.toArray)(binary));
+    const resultArray = (0, _1.toArray)(binary);
+    const epsilon = 0.0000001;
+    for (let i = 0; i < array.length; i++) {
+        (0, node_assert_1.default)(Math.abs(array[i] - resultArray[i]) < epsilon, new Error("Expected " +
+            `Math.abs(${array[i]} - ${resultArray[i]}) < ${epsilon}, ` +
+            `but was ${Math.abs(array[i] - resultArray[i])}`));
+    }
 });
--- index.test.ts
+++ index.test.ts
@@ -1,9 +1,21 @@
+import assert from "node:assert";
 import test from "node:test";
 import { toArray, toBinary } from ".";
-import assert from "node:assert";
 
 test("should work correctly", () => {
-  const array = new Array(1000).fill(0.5);
+  const array = new Array(100000).fill(0).map(() => Math.random());
   const binary = toBinary(array);
-  assert.deepStrictEqual(array, toArray(binary));
+  const resultArray = toArray(binary);
+  const epsilon = 0.0000001;
+
+  for (let i = 0; i < array.length; i++) {
+    assert(
+      Math.abs(array[i] - resultArray[i]) < epsilon,
+      new Error(
+        "Expected " +
+          `Math.abs(${array[i]} - ${resultArray[i]}) < ${epsilon}, ` +
+          `but was ${Math.abs(array[i] - resultArray[i])}`
+      )
+    );
+  }
 });
